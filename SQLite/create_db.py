ffdf8f3892528b7a233624d92ec5cf8db2603ed4
Jorge <jorgejapao00@gmail.com>
2024-07-25 23:03:19 -0300

    Database creation updated

--- SQLite/create_db.py
+++ SQLite/create_db.py
@@ -4,7 +4,7 @@ def create_db():
     connection = sqlite3.connect('SXwTRV21F0lC.db')
     cursor = connection.cursor()
 
-    cursor.execute("CREATE TABLE IF NOT EXISTS pssws(service, login, password)")
+    cursor.execute("CREATE TABLE IF NOT EXISTS pssws(service TEXT NOT NULL, login TEXT NOT NULL, password TEXT NOT NULL)")
 
 if __name__ == '__main__':
     create_db()
