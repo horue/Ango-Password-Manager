06f284d500f4ca42ce69874ee88a7b47ef97c6c5
Jorge <jorgejapao00@gmail.com>
2024-07-25 22:57:19 -0300

    Add password to db function created

---
+++ SQLite/add_password.py
@@ -0,0 +1,11 @@
+import sqlite3
+
+
+def add_password_db(service, login, password):
+    connection = sqlite3.connect('SXwTRV21F0lC.db')
+    cursor = connection.cursor()
+
+
+    cursor.execute(f'''INSERT INTO pssws(service, login, password) VALUES({service},{login},{password})''')
+    connection.commit()
+    connection.close()
