732efa40710403e400cc26f246f91069a7f37dc9
Jorge <jorgejapao00@gmail.com>
2024-07-26 23:42:30 -0300

    Auto pad added

--- GUI/gui.py
+++ GUI/gui.py
@@ -7,6 +7,9 @@ def main(root):
     b1 = ct.CTkButton(root, text='Add new password', command=lambda:toplevel.add_pass())
     b1.pack()
 
+    for child in root.winfo_children():
+        child.pack_configure(padx=10, pady=10)
+
 def s1():
     root = ct.CTk()
     root.geometry("700x650")
