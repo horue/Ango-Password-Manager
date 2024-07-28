f72df0b0746764b2f8dfde5a92f6ff560edcac3f
Jorge <jorgejapao00@gmail.com>
2024-07-27 23:40:27 -0300

    Pady from Serveice updated

--- GUI/add.py
+++ GUI/add.py
@@ -10,7 +10,7 @@ class toplevel():
         tl.grab_set()
 
         e1 = ct.CTkEntry(tl, placeholder_text='Service')
-        e1.pack()
+        e1.pack(pady=(300, 0))
 
         e2 = ct.CTkEntry(tl, placeholder_text='Login')
         e2.pack()
@@ -18,7 +18,7 @@ class toplevel():
         e3 = ct.CTkEntry(tl, placeholder_text='Password')
         e3.pack()
 
-        b1 = ct.CTkButton(tl, text='Add')
+        b1 = ct.CTkButton(tl, text='Add' """add_password.add_password_db(e1.get(), e2.get(), e3.get())""")
         b1.pack()
         
         for child in tl.winfo_children():
