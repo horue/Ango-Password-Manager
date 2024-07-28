783923f25365bd8b9d841c640750dcab2e6212cf
Jorge <jorgejapao00@gmail.com>
2024-07-28 17:29:48 -0300

    Add password imported from GUI folder

--- GUI/add.py
+++ GUI/add.py
@@ -1,5 +1,10 @@
 import customtkinter as ct
+import sys
+import os
 
+
+
+from add_password import *
 
 class toplevel():
     def add_pass(title='Add new password', size='500x400'):
@@ -18,7 +23,7 @@ class toplevel():
         e3 = ct.CTkEntry(tl, placeholder_text='Password')
         e3.pack()
 
-        b1 = ct.CTkButton(tl, text='Add' """add_password.add_password_db(e1.get(), e2.get(), e3.get())""")
+        b1 = ct.CTkButton(tl, text='Add', command=lambda:add_password_db(e1.get(), e2.get(), e3.get()))
         b1.pack()
         
         for child in tl.winfo_children():
