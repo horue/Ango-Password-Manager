6e7c599d4c880f680f2172d8197a34b5f2dbbab5
Jorge <jorgejapao00@gmail.com>
2024-07-29 22:38:27 -0300

    Visual adjustment at add new password screen

--- GUI/add.py
+++ GUI/add.py
@@ -13,12 +13,13 @@ class toplevel():
         tl.title(title)
         tl.resizable(False, False)
         tl.grab_set()
+        tl.iconbitmap('GUI\Visual\icon.ico')
 
         l1 = ct.CTkLabel(tl, text='Add new password')
         l1.pack()
 
         e1 = ct.CTkEntry(tl, placeholder_text='Service')
-        e1.pack(pady=(300, 0))
+        e1.pack()
 
         e2 = ct.CTkEntry(tl, placeholder_text='Login')
         e2.pack()
@@ -26,7 +27,7 @@ class toplevel():
         e3 = ct.CTkEntry(tl, placeholder_text='Password')
         e3.pack()
 
-        b1 = ct.CTkButton(tl, text='Add', command=lambda:add_password_db(e1.get(), e2.get(), e3.get()))
+        b1 = ct.CTkButton(tl, text='Add', command=lambda:add_password_db(e1.get(), e2.get(), e3.get()), fg_color='#343d44', hover_color='#676c74')
         b1.pack()
         
         for child in tl.winfo_children():
