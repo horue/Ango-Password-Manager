4f36748513ae71dbd59f2c2c811a7d7420932460
Jorge <jorgejapao00@gmail.com>
2024-08-14 21:10:54 -0300

    Minor code adjustment

--- GUI/add_password.py
+++ GUI/add_password.py
@@ -11,7 +11,3 @@ def add_password_db(service, login, password):
     ''', (service, login, password))
     connection.commit()
     connection.close()
-
-if __name__ == '__main__':
-    add_password_db(service='YT',login='email',password='senha')
-    add_password_db(service='KR',login='e',password='ef')
